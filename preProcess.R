313923da9f524d3a8c04ce6329ee66a073c810e0
Wush Wu <wush978@gmail.com>
2012-11-09 19:53:18 +0800

    parse list to data.frame v1

--- preProcess.R
+++ preProcess.R
@@ -1,1 +1,31 @@
 load("realprice.Rdata")
+# gc()
+# sapply(realprice, names)[,1]
+# sapply(realprice, function(a) names(a$fields))
+# x <- realprice[[1]]
+n <- length(realprice)
+retval <- NULL
+for(i in 1:n) {
+	x <- realprice[[i]]
+	y <- data.frame(
+		unit = x$fields[["交易單價 約"]],
+		latitude = as.numeric(x$pos[2]),
+		longitude = as.numeric(x$pos[1]),
+		address = x$address,
+		deal_year = x$fields[["交易年月"]]$year,
+		deal_month = x$fields[["交易年月"]]$month,
+		schema = x$fields[["建物現況格局"]],
+		is_manager = x$fields[["有無管理組織"]],
+		building_type = x$fields[["建物型態"]],
+		material = x$details[[3]][["主要建材"]],
+		seq = x$caseSeq,
+		no = x$caseNo
+		)
+	if(is.null(retval)) {
+		retval <- y
+	} else {
+		retval <- rbind(retval, y)
+	}
+}
+realprice <- retval
+save(realprice, file="realprice.dataframe.Rdata")
